83bcb4aead1c997f5a1d8738ada8cf17d30f83f7
Ryuma Maeshiro <e165730@ie.u-ryukyu.ac.jp>
2017-12-14 11:36:41 +0900

    Initial Commit

---
+++ margeTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  margeTest
+//
+//  Created by 眞榮城隆守 on 12/14/29 H.
+//  Copyright © 29 Heisei jp.ac.uryukyu.ie.e16530. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ margeTestTests/margeTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  margeTestTests.swift
+//  margeTestTests
+//
+//  Created by 眞榮城隆守 on 12/14/29 H.
+//  Copyright © 29 Heisei jp.ac.uryukyu.ie.e16530. All rights reserved.
+//
+
+import XCTest
+@testable import margeTest
+
+class margeTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
