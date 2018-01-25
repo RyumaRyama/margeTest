94408729ef54983952275637746218291860eb39
nagata <e145733@gmail.com>
2018-01-25 10:31:15 +0900

    a

---
+++ margeTest/NextViewController.h
@@ -0,0 +1,13 @@
+//
+//  NextViewController.h
+//  margeTest
+//
+//  Created by nagata on 2018/01/11.
+//  Copyright © 2018年 jp.ac.uryukyu.ie.e16530. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NextViewController : UIViewController
+
+@end
---
+++ margeTest/NextViewController.m
@@ -0,0 +1,37 @@
+//
+//  NextViewController.m
+//  margeTest
+//
+//  Created by nagata on 2018/01/11.
+//  Copyright © 2018年 jp.ac.uryukyu.ie.e16530. All rights reserved.
+//
+
+#import "NextViewController.h"
+
+@interface NextViewController ()
+
+@end
+
+@implementation NextViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
